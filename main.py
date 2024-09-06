616fd1bf8741f171218fdbcffe4a7c5e62003752
Johannes Losert <jal2340@columbia.edu>
2024-09-06 13:54:42 -0400

    added cardratings html

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from creditcard import CreditCard
 from scrape import get_card_dict
 
-cc_dict = get_card_dict('/home/johannes/CreditCards/cardratings/cardratings.html', None)
+cc_dict = get_card_dict('cardratings/cardratings.html', 'cardratings/output.txt')
 credit_cards = CreditCard.init_from_cc_dict(cc_dict)
 print(credit_cards)
