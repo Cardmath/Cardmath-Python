5cb2efb6dccb1c3006591497bda20164375b4221
Johannes Losert <jal2340@columbia.edu>
2024-09-07 20:05:44 -0400

    finish todo

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import random
 
 cc_dict = get_card_dict('cardratings/cardratings.html', 'cardratings/output.txt')
 
-# TODO ALLOW FOR RANDOM SAMPLES
-credit_cards = CreditCard.init_from_cc_dict(credit_card_dict=cc_dict, max_num=5, random_samples=True)
+credit_cards = CreditCard.init_from_cc_dict(credit_card_dict=cc_dict, max_num=2, random_samples=True)
 for credit_card in credit_cards:
     print(credit_card)
     print("---" * 10)
