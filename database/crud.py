84b1f51dc13d769c172db340109723b60aa39272
Johannes Losert <jal2340@columbia.edu>
2024-09-11 15:17:17 -0400

    basic db changes, naming

--- database/crud.py
+++ database/crud.py
@@ -1,6 +1,6 @@
 from . import models
 from sqlalchemy.orm import Session
-import schemas
+from typing import List
 
 def create_credit_card(db: Session, cc : models.CreditCard) -> bool:
     try: 
@@ -10,17 +10,21 @@ def create_credit_card(db: Session, cc : models.CreditCard) -> bool:
         return True
     except: 
         return False
+    
+def create_cardratings_scrape(db: Session, cc_scrape : models.CardratingsScrape) -> bool:
+    try: 
+        db.add(cc_scrape)
+        db.commit()
+        db.refresh(cc_scrape)
+        return True
+    except: 
+        return False
      
-
 def get_credit_card(db: Session, uid: str):
     return db.query(models.CreditCard).filter(models.CreditCard.uid == uid).first()
 
-def get_all_credit_cards(db: Session):
+def get_all_credit_cards(db: Session) -> List[models.CreditCard]:
     return db.query(models.CreditCard).all()
 
-def get_all_cardratings_scrapes(db: Session):
+def get_all_cardratings_scrapes(db: Session) -> List[models.CardratingsScrape]:
     return db.query(models.CardratingsScrape).all()
-
-# TODO Implement this function
-def create_cardratings_scrape(db: Session, cc : schemas.CreditCardCreate) -> bool:
-    return True
