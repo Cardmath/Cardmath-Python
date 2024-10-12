23bda1b23ea4f47ee0e2efa9a99a2b98c379bc1b
Johannes <jal2340@columbia.edu>
2024-10-12 16:50:36 -0400

    dashboard cleanup

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -20,7 +20,7 @@ const DashboardPage = () => {
             body: JSON.stringify({
                 account_ids: "all",
                 date_range: null,
-                window_size: 14,
+                window_size: 60,
                 top_n: 10
             })
         }).then(response => {
@@ -66,7 +66,7 @@ const DashboardPage = () => {
         <div id="app-sidebar-9" className="h-full lg:h-auto surface-section hidden lg:block flex-shrink-0 absolute lg:static left-0 top-0 z-1 border-right-1 surface-border w-18rem lg:w-7rem select-none">
             <div className="flex flex-column h-full">
                 <div className="flex align-items-center justify-content-center flex-shrink-0" style={{ height: '60px' }}>
-                    <img src="/demo/images/blocks/logos/hyper-cyan.svg" alt="Image" height="30" />
+                    <img src="/logo512.png" alt="Image" height="30" />
                 </div>
                 <div className="mt-3">
                     <ul className="list-none p-0 m-0">
@@ -80,7 +80,7 @@ const DashboardPage = () => {
                         <li>
                             <a className="p-ripple flex flex-row lg:flex-column align-items-center cursor-pointer p-3 lg:justify-content-center text-600 border-left-2 border-transparent hover:border-300 transition-duration-150 transition-colors">
                                 <i className="pi pi-search mr-2 lg:mr-0 mb-0 lg:mb-2 text-base lg:text-2xl"></i>
-                                <span className="font-medium inline text-base lg:text-xs lg:block">Search</span>
+                                <span className="font-medium inline text-base lg:text-xs lg:block">Search Cards</span>
                                 <Ripple />
                             </a>
                         </li>
@@ -146,7 +146,7 @@ const DashboardPage = () => {
                         <li>
                             <a className="p-ripple flex flex-row lg:flex-column align-items-center cursor-pointer p-3 lg:justify-content-center text-600 border-left-2 border-transparent hover:border-300 transition-duration-150 transition-colors">
                                 <i className="pi pi-cog mr-2 lg:mr-0 mb-0 lg:mb-2 text-base lg:text-2xl"></i>
-                                <span className="font-medium inline text-base lg:text-xs lg:block">Options</span>
+                                <span className="font-medium inline text-base lg:text-xs lg:block">Preferences</span>
                                 <Ripple />
                             </a>
                         </li>
@@ -163,65 +163,17 @@ const DashboardPage = () => {
             </div>
         </div>
         <div className="min-h-screen flex flex-column relative flex-auto">
-            <div className="flex justify-content-between align-items-center px-5 surface-section relative lg:static border-bottom-1 surface-border" style={{ height: '60px' }}>
-                <div className="flex">
-                    <StyleClass nodeRef={btnRef12} selector="#app-sidebar-9" enterClassName="hidden" enterActiveClassName="fadeinleft" leaveToClassName="hidden" leaveActiveClassName="fadeoutleft" hideOnOutsideClick>
-                        <a ref={btnRef12} className="p-ripple cursor-pointer block lg:hidden text-700 mr-3">
-                            <i className="pi pi-bars text-4xl"></i>
-                            <Ripple />
-                        </a>
-                    </StyleClass>
-                    <span className="p-input-icon-left">
-                        <i className="pi pi-search"></i>
-                        <InputText className="border-none w-10rem sm:w-20rem" placeholder="Search" />
-                    </span>
-                </div>
-                <StyleClass nodeRef={btnRef13} selector="@next" enterClassName="hidden" enterActiveClassName="fadein" leaveToClassName="hidden" leaveActiveClassName="fadeout" hideOnOutsideClick>
-                    <a ref={btnRef13} className="p-ripple cursor-pointer block lg:hidden text-700">
-                        <i className="pi pi-ellipsis-v text-2xl"></i>
-                        <Ripple />
-                    </a>
-                </StyleClass>
-                <ul className="list-none p-0 m-0 hidden lg:flex lg:align-items-center select-none lg:flex-row
-        surface-section border-1 lg:border-none surface-border right-0 top-100 z-1 shadow-2 lg:shadow-none absolute lg:static">
-                    <li>
-                        <a className="p-ripple flex p-3 lg:px-3 lg:py-2 align-items-center text-600 hover:text-900 hover:surface-100 font-medium border-round cursor-pointer
-                transition-duration-150 transition-colors">
-                            <i className="pi pi-inbox text-base lg:text-2xl mr-2 lg:mr-0"></i>
-                            <span className="block lg:hidden font-medium">Inbox</span>
-                            <Ripple />
-                        </a>
-                    </li>
-                    <li>
-                        <a className="p-ripple flex p-3 lg:px-3 lg:py-2 align-items-center text-600 hover:text-900 hover:surface-100 font-medium border-round cursor-pointer
-                transition-duration-150 transition-colors">
-                            <i className="pi pi-bell text-base lg:text-2xl mr-2 lg:mr-0 p-overlay-badge"><Badge severity="danger" /></i>
-                            <span className="block lg:hidden font-medium">Notifications</span>
-                            <Ripple />
-                        </a>
-                    </li>
-                    <li className="border-top-1 surface-border lg:border-top-none">
-                        <a className="p-ripple flex p-3 lg:px-3 lg:py-2 align-items-center hover:surface-100 font-medium border-round cursor-pointer
-                transition-duration-150 transition-colors">
-                            <img src="/demo/images/blocks/avatars/circle/avatar-f-1.png" alt="avatar-f-1" className="mr-3 lg:mr-0" style={{ width: '32px', height: '32px' }} />
-                            <div className="block lg:hidden">
-                                <div className="text-900 font-medium">Amy Elsner</div>
-                                <span className="text-600 font-medium text-sm">Marketing Specialist</span>
-                            </div>
-                            <Ripple />
-                        </a>
-                    </li>
-                </ul>
-            </div>
             <div className="p-5 flex flex-column flex-auto">
                 <div className="grid">
-                    <div className="col-12 lg:col-6">
+                    <div className="col-4 lg:col-4">
                         <div className="shadow-2 surface-card border-round p-3">
                             {heavyHittersCategories && heavyHittersCategories.length > 0 && <HeavyHitterPieChart heavyHitters={heavyHittersCategories} />}
                         </div>
                     </div>
-                    <div className="col-12 lg:col-6">
-                    <ChartSlider x={dates} y_list={categories} ready={isMovingAveragesReady} />
+                    <div className="col-8 lg:col-8">
+                        <div className="shadow-2 surface-card border-round p-3">
+                            <ChartSlider x={dates} y_list={categories} ready={isMovingAveragesReady} />
+                        </div>
                     </div>
                     <div className="col-12">
                         <div className="surface-ground">                            
