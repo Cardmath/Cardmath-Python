11f683c1152170ee81cc1f722581eb6986b5f255
Johannes <jal2340@columbia.edu>
2024-10-14 14:59:39 -0400

    use chips and scrollpanel for dataview

--- frontend/src/components/CreditCardFaceout.js
+++ frontend/src/components/CreditCardFaceout.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Card } from 'primereact/card';
 import { DataView } from 'primereact/dataview';
-import { Dropdown } from 'primereact/dropdown';
-        
+import { Chip } from 'primereact/chip';
+import { ScrollPanel } from 'primereact/scrollpanel';
 
 export default function CreditCardFaceouts() {
 
@@ -20,31 +20,29 @@ export default function CreditCardFaceouts() {
         .catch(error => console.log(error));
     }, []);
 
-    return (
-        <div className="w-full">   
-            <DataView className='gap-2'  
-                value={creditCards}
-                layout="grid"
-                itemTemplate={(e) => (
-                    <Card title={e.name} subTitle={e.issuer} footer={() => <div> <b> Credit Needed:</b> {e.credit_needed.join(', ')} </div>} className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2" key={e.id}>
-                        <div className="p-4 border-3 surface-border surface-card border-round">
-                            <p className="m-0"><b>Benefits:</b> {e.benefits.join(', ')} </p>
-                            <p className="m-0"><b>APR:</b> {e.apr} </p>
-                            <p className="m-0"> 
-                                <b>Reward Summary:</b>
-                                <ul className="m-0">
-                                    {e.reward_category_map.map(reward => (
-                                        <li key={reward.category}>{reward.category}: {reward.reward.reward_unit} {reward.reward.amount}</li>
-                                    ))}
-                                </ul>
-                            </p>
-                        </div>
-                    </Card>
-                    )}
-                paginator
-                rows={8}
-            />
-        </div>
+    return (   
+        <DataView  
+            value={creditCards}
+            layout="grid"
+            itemTemplate={(e) => (
+                <Card title={e.name} subTitle={e.issuer} footer={() => <div> <b> Credit Needed:</b> {e.credit_needed.join(', ')} </div>} className="col-12 sm:col-6 lg:col-12 xl:col-4" key={e.id}>
+                    <div className="border-3 surface-border surface-card border-round">
+                        {e.benefits.length === 0 ? <Chip className="bg-yellow-100" key="no_benefits" label="No benefits found" /> : <p className="m-0"><b>Benefits:</b> {e.benefits.map(benefit => <Chip className="bg-green-100" key={benefit} label={benefit} />)} </p>}
+                        <p className="m-0"><b>APR:</b> {e.apr} </p>
+                        <b>Reward Summary:</b>
+                        <ScrollPanel style={{ width: '100%', height: '200px' }}> 
+                            <ul className="m-0">
+                                {e.reward_category_map.map(reward => (
+                                    <li key={reward.category}>{reward.category}: {reward.reward.reward_unit} {reward.reward.amount}</li>
+                                ))}
+                            </ul>
+                        </ScrollPanel>
+                    </div>
+                </Card>
+                )}
+            paginator
+            rows={9}
+        />
     );
 }
 
