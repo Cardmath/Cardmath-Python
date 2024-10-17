b771571f5a202ad8fb026d68aa560edbda2396b7
Johannes <jal2340@columbia.edu>
2024-10-17 17:10:27 -0400

    remove trey

--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -8,7 +8,7 @@ const Team = () => {
                 <div className="col-12 lg:col-3 pr-0 lg:pr-4">
                     <div className="text-900 text-5xl font-bold mb-3">Meet our team</div>
                     <p className="text-700 text-lg line-height-3">
-                    Our team includes Cornell, Columbia, and Ohio State Honors students, a 2x founder, and interns from Google and Amazon, with expertise in business, algorithms, and software engineering.
+                        We are a Columbia University-based startup founded by undergraduates with a strong background in computer science and algorithms. Our team brings together expertise from leading tech companies and top research labs. Focused on cutting-edge technology and data-driven solutions, we are dedicated to helping users find the optimal credit card based on their transaction history and preferences.
                     </p>
                 </div>
                 <div className="col-12 lg:col-8">
@@ -18,17 +18,6 @@ const Team = () => {
                             <div className="font-medium text-xl mb-1 text-900">Johannes Losert</div>
                             <span className="text-600 font-medium">Founder, Engineering & Business</span>
                             <p className="line-height-3 mt-3 mb-3">Senior @ Columbia - Google, Amazon Intern. Has done AI Security research at NIST and Software Engineering at Google, Amazon.</p>
-                            <div className="mb-2">
-                                <a tabIndex="0" className="cursor-pointer"><i className="pi pi-twitter text-600 text-xl mr-3"></i></a>
-                                <a tabIndex="0" className="cursor-pointer"><i className="pi pi-github text-600 text-xl mr-3"></i></a>
-                                <a tabIndex="0" className="cursor-pointer"><i className="pi pi-facebook text-600 text-xl"></i></a>
-                            </div>
-                        </div>
-                        <div className="col-12 lg:col-6 p-3">
-                            <img src="/people/trey.jpeg" className="image-class mb-4 w-full" alt="team-2" />
-                            <div className="font-medium text-xl mb-1 text-900">Maitreya Dixit</div>
-                            <span className="text-600 font-medium">Co-Founder, Engineering</span>
-                            <p className="line-height-3 mt-3 mb-3">Senior @ tOSU Honors, 3x Google Intern. Aspiring software engineer with ambitions in the fields of high performance computing and machine learning.</p>
                             <div className="mb-2">
                                 <a tabIndex="0" className="cursor-pointer"><i className="pi pi-twitter text-600 text-xl mr-3"></i></a>
                                 <a tabIndex="0" className="cursor-pointer"><i className="pi pi-github text-600 text-xl mr-3"></i></a>
