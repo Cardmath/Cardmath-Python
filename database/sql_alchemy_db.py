8737c2970671dfe63e229bb897d000cf9653fbfe
Johannes <jal2340@columbia.edu>
2024-11-16 19:19:55 -0500

    configure database connection certificates on google cloud

--- database/sql_alchemy_db.py
+++ database/sql_alchemy_db.py
@@ -3,20 +3,32 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.schema import CreateTable
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ENVIRONMENT = os.getenv("ENVIRONMENT", "prod")
 
 if ENVIRONMENT == "prod":
-    # Production configuration (Google Cloud SQL)
+    # Production configuration (Google Cloud SQL with TLS)
     DB_NAME = os.getenv("DB_NAME", "postgres")
     DB_USER = os.getenv("DB_USER", "cardmathdb")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "your_database_password")
     DB_HOST = os.getenv("DB_HOST", "cardmath-llc:us-central1:cardmathdb")  # Instance connection name
     HOST_PREFIX = "/cloudsql"
 
-    # SQLAlchemy database URLs for async and sync connections
-    SQLALCHEMY_DATABASE_URL_ASYNC = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host={HOST_PREFIX}/{DB_HOST}"
-    SQLALCHEMY_DATABASE_URL_SYNC = f"postgresql://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host={HOST_PREFIX}/{DB_HOST}"
+    SSL_CERT_PATH = os.getenv("SSL_CERT_PATH")
+    SSL_KEY_PATH = os.getenv("SSL_KEY_PATH")
+    SSL_ROOT_CERT_PATH = os.getenv("SSL_ROOT_CERT_PATH")
+
+    SQLALCHEMY_DATABASE_URL_ASYNC = (
+        f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?"
+        f"host={HOST_PREFIX}/{DB_HOST}&sslmode=verify-full&sslcert={SSL_CERT_PATH}&sslkey={SSL_KEY_PATH}&sslrootcert={SSL_ROOT_CERT_PATH}"
+    )
+    SQLALCHEMY_DATABASE_URL_SYNC = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?"
+        f"host={HOST_PREFIX}/{DB_HOST}&sslmode=verify-full&sslcert={SSL_CERT_PATH}&sslkey={SSL_KEY_PATH}&sslrootcert={SSL_ROOT_CERT_PATH}"
+    )
 
 else:
     # Local development configuration (SQLite)
