4ea581616b3b19a81d41953300236ad8fab19c16
Johannes <jal2340@columbia.edu>
2024-12-01 17:30:29 -0500

    updated team page

--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -8,20 +8,30 @@ const Team = () => {
                 <div className="col-12 lg:col-3 pr-0 lg:pr-4">
                     <div className="text-900 text-5xl font-bold mb-3">Meet the Founder</div>
                     <p className="text-700 text-lg line-height-3">
-                        Cardmath was founded by Johannes Losert, a senior at Columbia University with experience at Amazon and Google. Johannes has led all aspects of Cardmath's development, from backend and frontend architecture to advanced algorithms, creating a platform that helps users maximize their credit card rewards.
-                        As a solo founder, Johannes has brought Cardmath to life, but we're seeking talented cofounders with engineering and design skills to help us progress. If you're passionate about simplifying personal finance, let's connect!
+                        Founders Nick Eliacin and Johannes Losert met during the Google STEP program in the summer of 2023. Their shared passion for technology and finance led them to create Cardmath—a user-friendly conversational assistant that provides personalized credit card recommendations and spending insights to simplify your credit journey.                    
                     </p>
                 </div>
-                <div className="col-12 lg:col-8">
+                <div className="col-6 lg:col-6">
                     <div className="grid">
                         <div className="col-12 lg:col-6 p-3">
                             <img src="/people/johannes.jpg" className="image-class w-full mb-3" alt="team-1"/>
                             <div className="font-medium text-xl mb-1 text-900">Johannes Losert</div>
-                            <span className="text-600 font-medium">Founder & CEO</span>
+                            <span className="text-600 font-medium">Co-Founder</span>
                             <p className="line-height-3 mt-3 mb-3">Senior at Columbia University. Ex Google, Amazon, National Institute of Standards & Technology Intern. Passionate about algorithms and software engineering. </p>
                             <div className="mb-2">
                                 <a onClick={() => window.open('https://github.com/johannes-losert/')} tabIndex="0" className="cursor-pointer"><i className="pi pi-github text-600 text-xl mr-3"></i></a>
                                 <a onClick={() => window.open('https://www.linkedin.com/in/johannes-losert/')} tabIndex="0" className="cursor-pointer"><i className="pi pi-linkedin text-600 text-xl"></i></a>
+                            </div>
+                        </div>
+                        <div className="col-6 lg:col-6">
+                            <img src="people/Eliacin_Nicholas_GeorgiaInstituteOfTechnology.jpg" className="image-class w-full mb-3" alt="team-1"/>
+                            <div className="font-medium text-xl mb-1 text-900">Nick Eliacin</div>
+                            <span className="text-600 font-medium">Co-Founder</span>
+                            <p className="line-height-3 mt-3 mb-3">
+                                Senior at Georgia Institute of Technology. Ex Microsoft, Google, and Equifax Intern. Passionate about data analysis in finance.                             </p>
+                            <div className="mb-2">
+                                <a onClick={() => window.open('https://github.com/gtnick19')} tabIndex="0" className="cursor-pointer"><i className="pi pi-github text-600 text-xl mr-3"></i></a>
+                                <a onClick={() => window.open('https://www.linkedin.com/in/neliacin3/')} tabIndex="0" className="cursor-pointer"><i className="pi pi-linkedin text-600 text-xl"></i></a>
                             </div>
                         </div>
                     </div>
